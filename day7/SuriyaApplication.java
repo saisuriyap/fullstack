184a6ff98a43990660e342c06584b0162b7d0f13
Suriya P <113583831+saisuriyap@users.noreply.github.com>
2024-02-09 10:32:48 +0530

    Add files via upload

---
+++ day7/SuriyaApplication.java
@@ -0,0 +1,13 @@
+package com.party.suriya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuriyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuriyaApplication.class, args);
+	}
+
+}
---
+++ day7/dto/response/EventResponse.java
@@ -0,0 +1,14 @@
+package com.party.suriya.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class EventResponse {
+    private String message;
+}
